71ca48dd652743a2e97985cd8b707b025c18df1b
Nimrod Feldman <Nimrod.feldman@gmail.com>
2023-04-25 16:30:23 +0300

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+
+const API_KEY = process.env.REACT_APP_API_KEY
+const API_CALL_INTERVAL_IN_MILI = 5 * 1000
+const LOCATION = "JFK"
 
 function App() {
+  const [weather, setWeather] = useState(null);
+
+  const fetchWeatherData = async () => {
+    try {
+      const options = { method: 'GET', headers: { accept: 'application/json' } };
+      let response = await fetch(`https://api.tomorrow.io/v4/weather/realtime?location=${LOCATION}&apikey=${API_KEY}`, options)
+      response = await response.json()
+      setWeather(response.data);
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchWeatherData()
+    const interval = setInterval(() => {
+      fetchWeatherData();
+    }, API_CALL_INTERVAL_IN_MILI);
+
+    return () => clearInterval(interval)
+  }, []);
+
+  const getBackgroundColor = () => {
+    if (!weather) return;
+
+    if (weather.values.temperature > 15) {
+      return { color: 'green', message: 'All clear' };
+    } else if (weather.values.temperature <= 15 && weather.values.temperature > 5) {
+      return { color: 'orange', message: '[Extreme Cold] Keep You Warm (Hot Drinks/Soups)' };
+    } else {
+      return { color: 'red', message: '[Danger Cold] Stop the work and get inside' };
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: "100%", width: "100%" }}>
+      {weather ? (
+        <div style={{ backgroundColor: getBackgroundColor().color, height: "100%", width: "100%" }}>
+          <h1 style={{marginTop: "0"}}>Location: {LOCATION}</h1>
+          <h1>Current Temperature: {weather.values.temperature}°C</h1>
+          <h2>{getBackgroundColor().message}</h2>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
